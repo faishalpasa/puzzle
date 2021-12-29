4f8b8551d752f20430586838a0c6167a62d853f9
Pasa <mfaishal_pasa@hotmail.com>
2021-12-29 20:31:51 +0700

    first commit

---
+++ src/components/Board/BoardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import BoardView from './BoradView'
+
+const BoardContainer = () => {
+  const viewProps = {}
+  return (
+    <BoardView {...viewProps} />
+  )
+}
+
+export default BoardContainer
---
+++ src/components/Board/BoradView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Board.css'
+
+const BoradView = () => (
+  <div className="board">
+    asd
+  </div>
+)
+
+export default BoradView
